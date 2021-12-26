e94e414b0c08cb044fa3fbacf526b9d9afd34087
Sneha-codes <74462732+Sneha-codes@users.noreply.github.com>
2021-12-26 18:06:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+function ratechange()
+{
+  var rchng = document.getElementById("rate").value;
+  document.getElementById("rate-displayer").innerHTML = rchng + '%';
+}
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+  var p = document.getElementById("principal").value;
+  if(p<=0)
+  {
+    alert("enter a positive number greater than zero");
+    document.getElementById("prinicipal").focus();
+    document.getElementById("prinicipal").click();
+    return null;
+  }
+  var r = document.getElementById("rate").value;
+  var y =document.getElementById("years").value;
+  console.log(y);
+  var interest = p*r*y/100;
+  console.log(p*r);
+  var ndate = new Date();
+  newdate = ndate.getFullYear();
+  newdate+= parseInt(y);
+  document.getElementById("result").innerHTML = " If you deposit " + p + " at an interest rate of " + r +'% , ' + "you will receive an amount of " +interest + " by the year " +newdate ;
 }
-        
